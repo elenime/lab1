80bda6d71d576311639aa1e9bb90132f789c28d9
Eleni <emek7557@Students.vsu.edu>
2025-10-08 15:24:19 -0400

    final commit

--- app/src/main/java/com/example/wordle/FourLetterWordList.kt
+++ app/src/main/java/com/example/wordle/FourLetterWordList.kt
@@ -1,2 +1,16 @@
-package com.example.wordle 
+package com.example.wordle
 
+object FourLetterWordList {
+
+    private val words = listOf(
+        "LAVA", "BANK", "CODE", "DESK", "FISH",
+        "GOAT", "HELP", "JAVA", "KING", "LION",
+        "MIND", "NEST", "OPEN", "PORK", "QUIZ",
+        "ROAD", "STAR", "TIME", "USER", "VIEW",
+        "WORD", "ZERO", "TREE", "SHIP", "RAIN"
+    )
+
+    fun getRandomFourLetterWord(): String {
+        return words.random()
+    }
+}
